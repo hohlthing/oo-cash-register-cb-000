0135d0dfec21c26dbb3b1223a33933e17fadc7f9
michael hohl <mhohl@rocketmail.com>
2018-03-22 22:55:26 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+class cash_register
+  attr_accessor :total, :discount
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
+  end
+
+  def total
+    @total
+  end
+end
